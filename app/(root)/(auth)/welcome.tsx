1ebb249082346bad8744161648ec5cd959dfaabd
Mark Phillips <mphill05@gmail.com>
2024-08-25 13:44:00 -0500

    first commit

---
+++ app/(root)/(auth)/welcome.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const welcome = () => {
+  return (
+    <View className="flex-1 items-center justify-center pt-10">
+      <Text>Updated test</Text>
+    </View>
+  );
+};
+
+export default welcome;
